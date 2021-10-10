ae0e805feeab1be0d8cc03396c9cf75d8bdb99be
Kwachendoerfer <kai.wachendoerfer@cofinpro.de>
2021-10-10 10:56:10 +0200

    added correct naming for actions

--- src/app/state/todo.actions.ts
+++ src/app/state/todo.actions.ts
@@ -2,16 +2,16 @@ import { createAction, props } from '@ngrx/store';
 import { Todo } from '../model/todo';
 
 export const addTodo = createAction(
-    'Add Todo',
+    '[Item Edit] Add Todo',
     props<{ todo: Todo }>(),
 );
 
 export const toggleTodo = createAction(
-    'Toggle Todo',
+    '[Item View] Toggle Todo',
     props<{ todoId: number }>(),
 );
 
 export const updateTodo = createAction(
-    'Update Todo',
+    '[Item Edit] Update Todo',
     props<{ todo: Todo }>(),
 );

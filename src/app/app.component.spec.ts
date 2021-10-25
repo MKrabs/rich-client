32ef93683289e9946006943828da41bad5909fa2
Kwachendoerfer <kai.wachendoerfer@cofinpro.de>
2021-10-25 20:16:36 +0200

    removed usless testing code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,26 +15,6 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  class SomeService {
-    someFunction(): boolean {
-      return true;
-    }
-
-    someOtherFunction(lala: string): string {
-      return 'foo';
-    }
-  }
-
-  const someServiceMock = mock(SomeService);
-  const someServiceInstance = instance(someServiceMock);
-
-  when(someServiceMock.someFunction()).thenReturn(false);
-  when(someServiceMock.someOtherFunction(anything())).thenReturn('bar');
-
-  verify(someServiceMock.someFunction()).once();
-
-  verify(someServiceMock.someOtherFunction('test123')).once();
-
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;

6d412248c224316ba9da80b178c69613b42f51a5
Kwachendoerfer <kai.wachendoerfer@cofinpro.de>
2021-10-25 20:16:08 +0200

    some changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { anything, instance, mock, verify, when } from 'ts-mockito';
 
 describe('AppComponent', () => {
   beforeEach(waitForAsync(() => {
@@ -13,6 +14,26 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
+
+  class SomeService {
+    someFunction(): boolean {
+      return true;
+    }
+
+    someOtherFunction(lala: string): string {
+      return 'foo';
+    }
+  }
+
+  const someServiceMock = mock(SomeService);
+  const someServiceInstance = instance(someServiceMock);
+
+  when(someServiceMock.someFunction()).thenReturn(false);
+  when(someServiceMock.someOtherFunction(anything())).thenReturn('bar');
+
+  verify(someServiceMock.someFunction()).once();
+
+  verify(someServiceMock.someOtherFunction('test123')).once();
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
